5301d52a3afbedba1125c47f82da5f9c6e5bd2ae
sawaca96 <sawaca96@hanmail.net>
2021-10-05 23:52:13 +0900

    ✨ Add web-server

---
+++ main.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+  region  = "ap-northeast-2"
+  profile = "terraform-up"
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default     = 8080
+}
+
+resource "aws_instance" "example" {
+  ami                    = "ami-0ba5cd124d7a79612"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+	#!/bin/bash
+	echo "Hello, World" > index.html
+	nohup busybox httpd -f -p 8080 &
+	EOF
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_dns
+
+}
