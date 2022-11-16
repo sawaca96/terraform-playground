6bf4847fcea60ed2f414213e35bb11cb83465846
sawaca96 <sawaca96@gmail.com>
2022-11-16 22:30:52 +0900

    🔨 WIP azure tutorial

---
+++ tutorial-azure/main.tf
@@ -0,0 +1,27 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
+}
+
+
